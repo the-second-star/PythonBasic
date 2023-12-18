32537c3c459d27d02d87b26cd6910e0a5a7c2334
the-second-star <154100002+the-second-star@users.noreply.github.com>
2023-12-19 08:07:33 +0900

    Create main.py

---
+++ 01/main.py
@@ -0,0 +1,61 @@
+print("Hello World")
+print("My name is Helena")
+print("Michaela is dumber than Helena")
+print("banana hates Michaela")
+print("iloyebanana")
+print("왈맹이")
+print('나의 \'첫 코딩\' 입니다')
+print("나의 '첫 코딩'입니다.")
+print('철수가 인사한다. "안녕?"')
+print("철수가 인사한다. \"안녕?\"")
+print("안녕하세요!")
+print('"안녕!"하고 \'철수가\' 인사했다')
+print("월요일, 화요일, 수요일, 목요일, 금요일")
+print("월요일", "화요일", "수요일", "목요일", "금요일", sep=",")
+a=0o177
+print(a)
+print(1)
+print(123)
+print(-24)
+print(0)
+print(1.2)
+print(123.24)
+print(-24.1)
+a = 3
+b = 4
+print(a + b) # 7
+print(a - b) # -1
+print(a * b) # 12
+print(a / b) # 0.75
+print(a ** b) # 81
+a = 7
+b = 4
+print(a / b) # 1.75
+print(a // b) # 1
+a = 7
+b = 3
+print(a % b) # 1
+print(b % a) # 3
+print(-a) # -7
+print(-b) # -3 
+a = 80
+b = 75
+c = 65
+print((a+b+c)/3)
+# d = a + b + c
+# print (d/3)
+a = 80000
+b = 3
+print(a // b) # 몫
+print(a % b) # 나머지
+#나머지 연산자를 활용
+print(1 % 2) # 1
+print(2 % 2) # 0
+print(3 % 2) # 1
+print(4 % 2) # 0
+print(2863 % 3) # ?
+print(29472 % 7) # ?
+print(3573920 % 7) # ?
+# 도로와의 거리 12m
+distance = 12
+print(0.2467 * distance + 4.159)
