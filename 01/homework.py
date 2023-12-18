38e4fbc22b87ab5fdd6be08b603fe3d4755cc246
the-second-star <154100002+the-second-star@users.noreply.github.com>
2023-12-19 08:08:34 +0900

    Create homework.py

---
+++ 01/homework.py
@@ -0,0 +1,46 @@
+print ("안녕하세요. 오늘의 날씨는 햇빛이 쨍쨍한 날입니다.")
+print ("지나가던 한 사람이 물었다.")
+print ('"실례합니다. 여의도역으로 가는 길이 어디입니까?"')
+print ("질문 받은 사람이 대답했다.")
+print ('"I do not know."')
+#1
+a = 14
+b = 3
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+print (a % b)
+#2
+a = 263 
+b = 38
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+print (a % b)
+a = 10000
+b = 25000
+c = 30000
+d = a + b + c
+print (d), print (d/3)
+price1 = 1000
+price2 = 96900
+f = 50
+c = (f-32)/1.8
+print (c)
+# (a + b)/c = (a/c) + (b/c)
+# (a - b)/c = (a/c) - (b/c)
+a = 2022
+b = 5
+c = 8
+print ((a + b)/c)
+print ((a/c) + (b/c))
+print ((a - b)/c)
+print ((a/c) - (b/c))
+a = 472
+b = 385
+n3 = 2360
+n4 = 3776
+n5 = 1416
+n6 = 181720
