988874de2cd5d3631f3f2d61bd496d36a0ee7893
the-second-star <154100002+the-second-star@users.noreply.github.com>
2023-12-19 15:41:22 +0900

    Create homework.py

---
+++ 02/homework.py
@@ -0,0 +1,58 @@
+# age 라는 이름 가진 변수를 만들고
+# 11 이라는 값을 할당해주세요.
+age = 11
+print (age)
+print ('안녕 파이썬!')
+print ("안녕 파이썬!")
+# print (''안녕 파이썬!'') : print 불가능
+# print (""안녕 파이썬!"") : print 불가능
+print ('''안녕 파이썬!''')
+print ("""안녕 파이썬!""")
+s1 = "변수"
+s2 = "할당"
+s3 = s1 + ' ' + s2 + ' '
+print (s3 * 3)
+sentence = "여의도역으로 가는 길은 어디인가요?"
+print (sentence[5])
+print (sentence[-5])
+print (sentence[10:12])
+print (sentence[13:])
+# s.format(____) 혹은 f'____' 사용
+s = '오늘의 평균 기온은 -9.4C֯ 도 입니다.'
+print (s)
+s = "나는 사과 {}개를 먹었다.".format(3)
+print (s)
+print ("나는 사과 {}개를 먹었다.".format(5))
+print ("나는 사과 {}개를 먹었다.".format("열"))
+print ("나는 사과 {}개를 먹었다.".format(100))
+print ("나는 사과 {}개를 먹었다.".format("이백"))
+# print("나는 사과 {}개를 먹었다.".format(다섯)): 오류 이유= 다섯에 " 나 ' 가 없어서
+s1 = "나는 오늘 학교에 갔다."
+s2 = "나는 오늘 학원에 갔다."
+s3 = "나는 오늘 놀이동산에 갔다."
+print (s1)
+print (s2)
+print (s3)
+age = 11
+print (age [0])
+age = '11'
+print (age [0])
+a = ''
+b = "Nice to meet you."
+c = 2023
+d = '''안녕하세요^^'''
+e = '2023/10'
+f = """Hello world!"""
+g = 202310
+h = 123
+i = '123'
+j = 0
+k = '''프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다.'''
+l = "\n"
+a = 2023
+b = 12
+print(a + b) # 2035
+a = "2023"
+b = "12"
+print(a + b) # 202312
