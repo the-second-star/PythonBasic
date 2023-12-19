aeb188d26ab7bceeb5e06fbcd5f6015a44f0dc23
the-second-star <154100002+the-second-star@users.noreply.github.com>
2023-12-19 09:59:09 +0900

    Create main.py

---
+++ 02/main.py
@@ -0,0 +1,80 @@
+year_of_birth = 2010
+# 2022년 나이
+print (2022 - year_of_birth)
+# 2023년 나이
+print (2023 - year_of_birth)
+# 2024년 나이
+print (2024 - year_of_birth)
+# 2033년 나이
+print (2033 - year_of_birth)
+red_pen_price = 1000
+pencil_price = 500
+eraser_price = 700
+black_pen_price = 1500
+total_price = red_pen_price * 3 + pencil_price * 2 + eraser_price * 1 +black_pen_price * 4
+print (total_price)
+age = 13
+print (age)
+number_of_notebook = 4
+number_of_pencil_case = 1
+number_of_electronics = 4
+number_of_food = 1
+print (number_of_electronics + number_of_food + number_of_notebook + number_of_pencil_case)
+print ('안녕 파이썬!')
+print ("안녕 파이썬!")
+print ('''오늘은 파이썬 공부하는 날''')
+print ("""오늘은 파이썬 공부하는 날""")
+print ('프로그래밍 언어는 다양하다. \n파이썬은 프로그래밍 언어이다.')
+print ('''프로그래밍 언어는 다양하다. 
+파이썬은 프로그래밍 언어이다.''')
+string = """프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다."""
+print (string)
+print ("\"파이썬은 쉽다\"라고 '철수'가 말했어요.")
+print ('"파이썬은 쉽다"라고 \'철수\'가 말했어요.')
+print ("\"파이썬은 쉽다\"라고 \'철수\'가 말했어요.")
+print ("""파이썬은 쉽다"라고 '철수'가 말했어요.""")
+print ('''파이썬은 쉽다"라고 '철수'가 말했어요.''')
+head = '오늘 날씨는'
+tail = '나쁘지 않다. 바나나는 밥을 먹지 않는다.'
+print (head + tail)
+print ('=' * 50)
+print ('My Programm')
+print ('=' * 50)
+s1='python'
+s2='java'
+s3 = s1 + ' ' + s2 + ' '
+print (s3 * 4)
+a = 3
+b = 4
+print (a + b)
+a = "3"
+b = "4"
+print (a + b)
+hello = '안녕하세요!'
+print (hello [2]) # 하
+print (hello [5])
+print (hello [-1])
+banana = '바나나나나나나나'
+print (banana)
+sentence = 'Love yourself.'
+print (sentence [0])
+print (sentence [2])
+print (sentence [7])
+print (sentence [9])
+print (sentence [12])
+phrase = '왈맹이'
+print (phrase)
+print ('is cuter than')
+print (banana)
+인사 = '안녕하세요!'
+print (인사 [2:4]) # 2번째 부터 4번째 전까지
+print (sentence [0:4])
+word = sentence [0:4]
+print (word)
+sentence = '안녕하세요. 저는 파이썬을 공부하고 있습니다. 지금은 문자열을 배우고 있습니다.'
+print (sentence [0:5])
+print (sentence [0:2])
+print (sentence [7:9])
+print (sentence [10:13])
+print (sentence [20:33])
